1167fb69fdec918317fb0a3273e95fb8f636d963
bruno-python <b.voisin@hotmail.fr>
2019-11-25 16:29:40 +0100

    modifier

--- arbre_turtle.py
+++ arbre_turtle.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from turtle import *
 angle = 30
 color('#3f1905')
@@ -25,32 +24,3 @@ left(180) # fait un demi-tour
 down() # pose le stylo
 arbre(11,700) # exécute la macro
 showturtle() # affiche la tortue
-=======
-from turtle import *
-angle = 30
-color('#3f1905')
-def arbre(n,longueur):
-    if n==0:
-        color('green')
-        forward(longueur) # avance
-        backward(longueur) # recule
-        color('#3f1905')
-    else:
-        width(n)
-        forward(longueur/3) #avance
-        left(angle) # tourne vers la gauche de angle degrés
-        arbre(n-1,longueur*2/3)
-        right(2*angle) # tourne vers la droite de angle degrés
-        arbre(n-1,longueur*2/3)
-        left(angle) # tourne vers la gauche de angle degrés
-        backward(longueur/3) # recule
-hideturtle() # cache la tortue
-up() # lève le stylo
-right(90) # tourne de 90 degrés vers la droite
-forward(300) # avance de 300 pixels
-left(180) # fait un demi-tour
-down() # pose le stylo
-arbre(11,700) # exécute la macro
-showturtle() # affiche la tortue
->>>>>>> 863526c6c0912961c871a9766612c29183e6d575
-mainloop()
